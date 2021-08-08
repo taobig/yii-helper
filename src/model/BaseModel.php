c898593d31b7d69f6d0fc8e5d36623401fb82653
taobig <studentht@hotmail.com>
2021-08-08 16:34:51 +0800

    Add taobig\yii\model\BaseModel

--- src/model/BaseModel.php
+++ src/model/BaseModel.php
@@ -1,89 +1,19 @@
 <?php
 
-namespace taobig\yii;
+namespace taobig\yii\model;
 
 use taobig\yii\exceptions\ActiveRecordSaveException;
 use taobig\yii\exceptions\UserException;
 use Yii;
 use yii\db\ActiveRecord;
 
-/**
- * @deprecated
- * @see \taobig\yii\model\BaseModel
- */
 abstract class BaseModel extends ActiveRecord
 {
-
-    /**
-     * @return string|null
-     */
-    public static function getSoftDeleteAttribute()
-    {
-        return 'deleted_at';
-    }
 
 //    public function optimisticLock()
 //    {
 //        return 'version';
 //    }
-
-    /**
-     * 区别于self::findActiveOne()，会加入limit(1)
-     * @param $condition
-     * @return static|null ActiveRecord instance matching the condition, or `null` if nothing matches.
-     * @throws \yii\base\InvalidConfigException
-     */
-    public static function findActiveOneLimit($condition)
-    {
-        $query = static::findByCondition($condition);
-        if (static::getSoftDeleteAttribute()) {
-            $query->andWhere([static::getSoftDeleteAttribute() => 0]);
-        }
-        return $query->limit(1)->one();
-    }
-
-    /**
-     * @param $condition
-     * @return static|null ActiveRecord instance matching the condition, or `null` if nothing matches.
-     * @throws \yii\base\InvalidConfigException
-     */
-    public static function findActiveOne($condition)
-    {
-        $query = static::findByCondition($condition);
-        if (static::getSoftDeleteAttribute()) {
-            $query->andWhere([static::getSoftDeleteAttribute() => 0]);
-        }
-        return $query->one();
-    }
-
-    /**
-     * @param $condition
-     * @return static[] an array of ActiveRecord instances, or an empty array if nothing matches.
-     * @throws \yii\base\InvalidConfigException
-     */
-    public static function findActiveAll($condition)
-    {
-        $query = static::findByCondition($condition);
-        if (static::getSoftDeleteAttribute()) {
-            $query->andWhere([static::getSoftDeleteAttribute() => 0]);
-        }
-        return $query->all();
-    }
-
-    /**
-     * @return false|int
-     * @throws \Throwable
-     * @throws \yii\db\StaleObjectException
-     */
-    public function softDelete()
-    {
-        $softDeleteAttribute = static::getSoftDeleteAttribute();
-        if ($softDeleteAttribute) {
-            $this->{$softDeleteAttribute} = time();
-            return $this->update(true, [$softDeleteAttribute]);
-        }
-        return false;
-    }
 
     public function getFirstErrorMessage(): string
     {
