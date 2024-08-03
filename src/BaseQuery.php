7455d162ab5df6551e4a5ffcc6762144c0a5e4e7
taobig <studentht@hotmail.com>
2024-08-03 22:22:23 +0800

    pick 老版本BaseQuery中active()

--- src/BaseQuery.php
+++ src/BaseQuery.php
@@ -7,31 +7,6 @@ namespace taobig\yii;
  */
 abstract class BaseQuery extends \taobig\yii\model\BaseQuery
 {
-    /*public function active()
-    {
-        return $this->andWhere('[[status]]=1');
-    }*/
-
-    /**
-     * @deprecated
-     * @param BaseModel $model
-     * @return $this
-     */
-    abstract public function search($model);
-
-    /**
-     * @deprecated
-     * @return $this
-     */
-    public final function active()
-    {
-        /** @var BaseModel $model */
-        $model = (new $this->modelClass);
-        if ($model->getSoftDeleteAttribute()) {
-            $this->andWhere([$model->getSoftDeleteAttribute() => 0]);
-        }
-        return $this;
-    }
 
     /**
      * @deprecated
--- src/model/BaseQuery.php
+++ src/model/BaseQuery.php
@@ -2,8 +2,32 @@
 
 namespace taobig\yii\model;
 
-
 abstract class BaseQuery extends \yii\db\ActiveQuery
 {
+
+    /**
+     * @param BaseModel $model
+     * @return $this
+     */
+    abstract public function search($model);
+
+
+    /*public function active()
+    {
+        return $this->andWhere('[[status]]=1');
+    }*/
+
+    /**
+     * @return $this
+     */
+    public final function active()
+    {
+        /** @var BaseModel $model */
+        $model = (new $this->modelClass);
+        if ($model->getSoftDeleteAttribute()) {
+            $this->andWhere([$model->getSoftDeleteAttribute() => 0]);
+        }
+        return $this;
+    }
 
 }
